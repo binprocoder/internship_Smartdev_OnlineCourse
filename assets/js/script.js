daadb82417e512bc6d128918fd5f9bd0acaf8c16
Bin Tran <kurochan159357@gmail.com>
2021-12-15 12:35:23 +0700

    use pagination js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,26 @@
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
 var listCoursesBlock = document.querySelector('#list-courses');
-var form = document.getElementById("form-login");
-var loginArea = document.getElementById("login-area");
-var logoutArea = document.getElementById("logout-area");
 let pagination = document.getElementById("pagination");
 
-// Pagination
-var limitItem = 4;
-const buttonGenerator = (limitItem, courses) => {
-  const nofTr = courses.length;
-  if (nofTr <= limitItem) {
 
-  }
-}
-window.onscroll = () => {
-  loginForm.classList.remove('active');
-}
-formBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  loginForm.classList.add('active');
-});
+// let currentPage = 1;
+// let perPage = 4;
+// const handlePage = (key)=> {
+//   currentPage = key;
+//   alert(key);
+// } 
 
-formClose.addEventListener('click', () => {
-  loginForm.classList.remove('active');
-});
+// // Pagination
+// const handlePagination = (courses) => {
 
+//   let totalPage = Math.ceil(courses.length / perPage);
+//   let perPost = [];
+
+//   for (let i = 1; i <= totalPage; i++) {
+//     document.getElementById('pagination').innerHTML += `<button onclick = handlePage(${i} +',' + ${courses})>${i}</button>`
+//   }
+// }
+
+// Swiper review
 var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   loop: true,
@@ -47,88 +41,54 @@ var swiper = new Swiper(".review-slider", {
   },
 });
 
-var userApi = 'https://61a0652fa647020017613372.mockapi.io/api/users';
-const getUsers = async (data) => {
-  await fetch(userApi)
-    .then(res => res.json())
-    .then(data);
-}
 
-// Get element from input
-const handleForm = (e) => {
-  e.preventDefault();
-  getUsers((users) => {
-    login(users);
-  });
-}
-form.addEventListener('submit', handleForm);
-
-// renderLogin
-const renderLogin = () => {
-  const userCurrent = JSON.parse(localStorage.getItem('userCurrent'));
-
-  const logout = {
-    username: null,
-    password: null
-  }
-  if (userCurrent.username == null && userCurrent.password == null) {
-    logoutArea.style.display = 'none';
-    loginArea.style.display = 'initial';
-  }
-  else {
-    loginForm.classList.remove('active');
-    loginArea.style.display = "none";
-    logoutArea.style.display = "initial";
-  }
-  logoutArea.onclick = () => {
-    localStorage.setItem('userCurrent', JSON.stringify(logout));
-  };
-}
-// Login authentication
-
-const login = (users) => {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  let flag = false;
-  for (let i = 0; i < users.length; i++) {
-    if (username == users[i].username && password == users[i].password) {
-      flag = true;
-      break;
+// Pagination js
+const handlePagination = (data) =>{
+  $('#pagination').pagination({
+    dataSource: data,
+    pageSize: 4,
+    callback: function(data, pagination) {
+        // template method of yourself
+        var newCourses = data.map(function (course) {
+          return `
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12"  >
+              <div class="course-item mb-3 mt-3" ">
+                <a href="https://hoclaptrinhonline.asia/course/view.php?id=22">
+                    <div class="img-item">
+                        <img class="img-course" src="${course.anh}"
+                            alt="Khóa học lập trình JavaScript">
+                    </div>
+                </a>
+                <div class="detail-item">
+                    <p>Updated ${course.date}</p>
+                    <a href="https://hoclaptrinhonline.asia/course/view.php?id=22">
+                        <h3>${course.name}</h3>
+                    </a>
+                    <p>${course.description}</p>
+                </div>
+                <hr />
+                <div class="footer-item">
+                    <i class="far fa-user"><span>${course.numberLearn}</span></i>
+                    <i class="far fa-comments"><span>${course.comment}</span></i>
+                    <p>Giá tiền: <span>${course.price}</span></p>
+                </div>
+              </div>
+            </div>
+          `;
+        });
+        const html = newCourses.join('');
+        document.getElementById('list-courses').innerHTML = html;
     }
-    else
-      flag = false;
-  }
-  console.log(flag);
-  if (password.length <= 6) {
-    alert("Password must be longer than 6 characters");
-  }
-  else {
-    if (flag) {
-      var user = {
-        username,
-        password,
-      }
-      localStorage.setItem('userCurrent', JSON.stringify(user));
-      renderLogin();
-      alert('Login successful');
-    }
-    else
-      alert('Wrong password');
-  }
-
-  // }
+  })
 }
-
-// ES6
-
-// Render
-
+// Get course data from JSON
 var courseApi = 'https://61a0652fa647020017613372.mockapi.io/api/courses';
 const getCourses = async (data) => {
   await fetch(courseApi)
     .then(res => res.json())
     .then(data);
 }
+// Render course data
 const renderCourse = (courses) => {
   var htmls = courses.map(function (course) {
     return `
@@ -151,6 +111,7 @@ const renderCourse = (courses) => {
           <div class="footer-item">
               <i class="far fa-user"><span>${course.numberLearn}</span></i>
               <i class="far fa-comments"><span>${course.comment}</span></i>
+              <p>Giá tiền: <span>${course.price}</span></p>
           </div>
         </div>
       </div>
@@ -167,15 +128,16 @@ const totalPrice = (courses) => {
   alert('Khuyến mãi hôm nay tổng tiền toàn bộ khóa học: ' + total + 'VND sẽ được giảm giá 20% chỉ còn lại: ' + giakhuyenmai + 'VND');
   return total;
 }
-// 
+// Khi bật trang web lên
 const start = () => {
   getCourses((courses) => {
     renderCourse(courses);
     let courseItem = document.getElementsByClassName("course-item");
     console.log(courseItem.length);
     totalPrice(courses);
+    handlePagination(courses);
   });
-  renderLogin();
+
 }
 start();
 
